9f91ef6752b0469ee309125d4999605cd2fd1513
muizidn <muiz.idn@gmail.com>
2020-01-30 21:17:54 +0700

    Fix #1 - muizidn

--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
-
 import (
 	"fmt"
 
+	"github.com/pondok-programmer/golang-learn/sidik"
 	"github.com/pondok-programmer/golang-learn/syarif"
-  	"github.com/pondok-programmer/golang-learn/sidik"
 )
 
 func main() {
 	fmt.Println("Start golang-learn")
 	syarif.Main()
-
-
 	sidik.Main()
-
-
 	fmt.Println("End golang-learn")
+
+	fmt.Println("Finished")
 }
