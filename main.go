f5a478659f8b2213c1cf3deb45beb8bd9d71e84f
Muhammad Sidik <pasker.shinigami@gmail.com>
2020-01-30 21:01:15 +0700

    Fix #1 - https://tour.golang.org/basics/1

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import "fmt"
+import "github.com/pondok-programmer/golang-learn/sidik"
 
 func main() {
 	fmt.Println("Start golang-learn")
+
+	sidik.Main()
+
 	fmt.Println("End golang-learn")
 }
