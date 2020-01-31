c7131992a76cbf3d2b20b602fead9d00b3221d2a
muizidn <muiz.idn@gmail.com>
2020-01-31 21:04:11 +0700

    separate print with space

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ import (
 
 func main() {
 	fmt.Println("Start golang-learn")
+  fmt.Println("Run Syarif")
 	syarif.Main()
+  fmt.Println()
+  fmt.Println("Run Sidik")
 	sidik.Main()
+  fmt.Println()
+  fmt.Println("Run Roihan")
 	roihan.Main()
+  fmt.Println()
 	fmt.Println("End golang-learn")
 }
