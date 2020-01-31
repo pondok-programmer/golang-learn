93c3faa5e7b6f7adefd74005cd71bbbe0a540a05
Muhammad Sidik <pasker.shinigami@gmail.com>
2020-01-31 09:43:27 +0700

    Fix #5 - https://tour.golang.org/basics/2

--- sidik/sidik.go
+++ sidik/sidik.go
@@ -2,6 +2,12 @@ package sidik
 
 import "fmt"
 import "math"
+
 func Main() {
+	fmt.Println("Hello World from Sidik")
+	basic1()
+}
+
+func basic1() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
