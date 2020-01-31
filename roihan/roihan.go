1f66ede61e982fe6f04dabd031c25fe8ed662d81
AlMishbah <roihanmisbahulanam140@gmail.com>
2020-01-31 14:41:04 +0700

    Fix #5 - https://tour.golang.org/basics/1

--- roihan/roihan.go
+++ roihan/roihan.go
@@ -3,12 +3,18 @@ package roihan
 
 import "fmt"
 import "math"
+import "math/rand"
 
 func Main()  {
 	fmt.Println("Hello World from Roihan")
 	basic1()
+	basics1()
 }
 
 func basic1()  {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
+
+func basics1() {
+	fmt.Println("My favorite number is" , rand.Intn(10))
 }
