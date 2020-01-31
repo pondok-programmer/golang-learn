389288770b35a68f699c5806b2e69b0c9af967c8
Syarif1922 <syarifhidayatullah.pk@gmail.com>
2020-01-31 14:08:48 +0700

    Fix #5 - https://tour.golang.org/basics/2

--- syarif/syarif.go
+++ syarif/syarif.go
@@ -2,8 +2,13 @@ package syarif
 
 import (
 	"fmt"
+	"math"
 )
 
 func Main() {
 	fmt.Println("Hello World from syarif")
+	basics1()
+}
+func basics1() {
+	fmt.Println("Now you have %g problems.\n", math.Sqrt(7))
 }
