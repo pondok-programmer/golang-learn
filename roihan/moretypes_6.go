12905bae26e5104e6d3e14213384ce155c851af9
AlMishbah <roihanmisbahulanam140@gmail.com>
2020-02-08 13:28:52 +0700

    Fix #34 - https://tour.golang.org/moretypes/6

---
+++ roihan/moretypes_6.go
@@ -0,0 +1,14 @@
+package roihan
+
+import "fmt"
+
+func moretype6() {
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 11, 13}
+	fmt.Println(primes)
+}
